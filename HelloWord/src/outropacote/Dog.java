1004e4cdeb97a270bc177fb72426d8ec1644cc05
Pedro Grigorio <pedroogrigorio@gmail.com>
2022-09-23 08:29:16 -0300

    first java program

---
+++ HelloWord/src/outropacote/Dog.java
@@ -0,0 +1,7 @@
+package outropacote;
+
+public class Dog {
+    public static void main(String[] args) {
+        System.out.println("Dog");
+    }
+}
---
+++ HelloWord/src/pacote/App.java
@@ -0,0 +1,7 @@
+package pacote;
+
+public class App {
+    public static void main(String[] args){
+        System.out.print("Olá, mundo");
+    }
+}
