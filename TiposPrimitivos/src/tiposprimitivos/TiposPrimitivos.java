a5e57f5a989862b8bc9498dcbb3955671e9b67d9
Pedro Grigorio <pedroogrigorio@gmail.com>
2022-09-25 09:55:58 -0300

    data types

---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,24 @@
+package src.tiposprimitivos;
+
+import java.util.Scanner;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) throws Exception {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.printf("Digite o nome do aluno: ");
+        String nome = teclado.nextLine();
+
+        System.out.printf("Digite a nota do aluno: ");
+        float nota = teclado.nextFloat();
+
+        System.out.printf("A nota de %s é %.1f \n", nome, nota);
+
+        String notaString = Float.toString(nota);
+        System.out.printf("Nota em string: %s \n", notaString);
+
+        float notaString2Integer = Float.parseFloat(notaString);
+        System.out.printf("String para float: %.1f",notaString2Integer);
+    }
+}
