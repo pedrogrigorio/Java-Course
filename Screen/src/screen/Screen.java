dbb80318dccf4eed05431336ba93a71615f5bf20
Pedro Grigorio <pedroogrigorio@gmail.com>
2022-09-28 12:23:03 -0300

    JLabel

--- Screen/src/screen/Screen.java
+++ Screen/src/screen/Screen.java
@@ -8,6 +8,7 @@ public class Screen extends JFrame {
 
     JTextField num1;
     JTextField num2;
+    JLabel title;
 
     public Screen() {
 
@@ -16,18 +17,18 @@ public class Screen extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
-
         setLayout(null);
-        JButton jButton = new JButton("Clique aqui");
-        jButton.setBounds(100, 200, 250, 70);
-        jButton.setFont(new Font("Arial", Font.BOLD, 40));
+
+        JButton jButton = new JButton("Soma");
+        jButton.setBounds(175, 350, 150, 70);
+        jButton.setFont(new Font("Arial", Font.BOLD, 20));
         jButton.setForeground(new Color(237, 241, 238));
         jButton.setBackground(new Color(9, 10, 9));
 
         JButton jButton2 = new JButton();
-        jButton2.setText("Outro botão");
-        jButton2.setBounds(0, 0, 150, 70);
-        jButton2.setFont(new Font("Arial", Font.ITALIC, 20));
+        jButton2.setText("Subtração");
+        jButton2.setBounds(375, 350, 150, 70);
+        jButton2.setFont(new Font("Arial", Font.BOLD, 20));
         jButton2.setForeground(new Color(237, 241, 238));
         jButton2.setBackground(new Color(9, 10, 9));
 
@@ -38,22 +39,41 @@ public class Screen extends JFrame {
          *jButton.addActionListener(this);
          *jButton2.addActionListener(this);
          */
+        
         jButton.addActionListener(this::teste);
         jButton2.addActionListener(this::outroteste);
 
         num1 = new JTextField("Clique aqui");
-        num1.setBounds(100, 100, 100, 100);
+        num1.setBounds(200, 200, 100, 100);
         num1.setFont(new Font("Arial", Font.ITALIC, 40));
         num1.setText("texto");
+        
+        add(num1);
 
         num2 = new JTextField("Clique aqui");
-        num2.setBounds(200, 100, 100, 100);
+        num2.setBounds(400, 200, 100, 100);
         num2.setFont(new Font("Arial", Font.ITALIC, 40));
         num2.setText("texto");
 
-        add(num1);
         add(num2);
 
+        JLabel num1Label = new JLabel("Numero 1");
+        num1Label.setBounds(220, 301, 90, 50);
+        num1Label.setFont(new Font("Arial", Font.PLAIN, 14));
+
+        add(num1Label);
+
+        JLabel num2Label = new JLabel("Numero 2");
+        num2Label.setBounds(420, 301, 90, 50);
+        num2Label.setFont(new Font("Arial", Font.PLAIN, 14));
+
+        add(num2Label);
+
+        title = new JLabel("CALCULADORA");
+        title.setBounds(200, 100, 400, 50);
+        title.setFont(new Font("Arial", Font.BOLD, 40));
+
+        add(title);
         setVisible(true);
     }
 
@@ -64,8 +84,14 @@ public class Screen extends JFrame {
     }
     */
 
-    private void teste(ActionEvent actionEvent){
-        JOptionPane.showMessageDialog(null, "Aviso!", "Status", JOptionPane.WARNING_MESSAGE);
+    private void teste(ActionEvent actionEvent){ 
+        Integer numero1 = Integer.parseInt(num1.getText());
+        Integer numero2 = Integer.parseInt(num2.getText());
+
+        Integer soma = numero1 + numero2;
+        title.setText("O resultado é: " + soma);
+
+        //JOptionPane.showMessageDialog(null, "Resultado da soma: " + soma, "Status", JOptionPane.WARNING_MESSAGE);
     }
 
     private void outroteste(ActionEvent actionEvent){
@@ -73,8 +99,8 @@ public class Screen extends JFrame {
         Integer numero1 = Integer.parseInt(num1.getText());
         Integer numero2 = Integer.parseInt(num2.getText());
 
-        Integer soma = numero1 + numero2;
-
-        JOptionPane.showMessageDialog(null, "Resultado = " + soma, "Error", JOptionPane.ERROR_MESSAGE);
+        Integer sub = numero1 - numero2;
+        title.setText("O resultado é: " + sub);
+        //JOptionPane.showMessageDialog(null, "Resultado da subtração: " + sub, "Error", JOptionPane.ERROR_MESSAGE);
     }
 }
